71c1154fe50830d61850f81f20ce015e220ca5d0
Franco Castro Miranda <fraanco.cm@gmail.com>
2016-03-31 16:21:27 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express"),  
+    app = express(),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
+    mongoose = require('mongoose');
+    cordova = require('cordova');
+
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
+app.use(methodOverride());
+
+var router = express.Router();
+
+router.get('/', function(req, res) {  
+   res.send("Hola mundo");
+});
+
+app.use(router);
+
+app.listen(3000, function() {  
+  console.log("Node server running on http://localhost:3000");
+});
